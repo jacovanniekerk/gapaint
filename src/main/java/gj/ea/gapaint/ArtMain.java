0a5b61ce90e90c277f24d8f8557e80a0dcff807f
Jaco van Niekerk <jaco.vanniekerk@rorotika.com>
2011-12-12 21:10:37 +0200

    Program entry working.
    * Added a bash script 'go.sh' to start the application (Windows version would
      be nice...)
    * Added main class to launch everything.
    * Added shade plugin to create executable jar.

---
+++ src/main/java/gj/ea/gapaint/ArtMain.java
@@ -0,0 +1,10 @@
+package gj.ea.gapaint;
+
+public class ArtMain {
+    
+    public static void main(String[] args) {
+        System.out.println("Ok, cool, it works...");
+        System.out.println("Now to get the GA working!");
+    }
+
+}
